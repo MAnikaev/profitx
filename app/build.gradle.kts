c6cb14eb612fa3cf4229b7c3bb2673d0fef03ff8
MAnikaev <anikaev.maks@gmail.com>
2025-02-08 02:21:36 +0300

    Test CI 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,10 +56,10 @@ detekt {
     // Can lead to speedups in larger projects. `false` by default.
     parallel = true
 
-    // Define the detekt configuration(s) you want to use.
-    // Defaults to the default detekt configuration.
-    config.setFrom("$rootDir/config.yml")
-    buildUponDefaultConfig = true
+//    // Define the detekt configuration(s) you want to use.
+//    // Defaults to the default detekt configuration.
+//    config.setFrom("$rootDir/config.yml")
+//    buildUponDefaultConfig = true
 }
 
 dependencies {
