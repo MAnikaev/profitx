43d9de7f33061f089e5a05ff9bc3e6992658c919
MAnikaev <anikaev.maks@gmail.com>
2025-02-08 02:39:39 +0300

    Test CI 13

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -61,10 +61,34 @@ detekt {
 
 dependencies {
 
+    implementation(libs.bundles.compose)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.collections.immutable)
+
+    implementation(libs.koin)
+    implementation(libs.koin.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.compose.charts)
+    implementation(libs.color.picker)
+    implementation(libs.color.picker.extensions)
+
+    // move to core
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.ui.tooling.preview.android)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.navigation.compose)
+    implementation(libs.material.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.ui.tooling)
 }
